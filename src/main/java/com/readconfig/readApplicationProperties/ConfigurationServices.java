ff3136ee654776bafe186200ca5e6f7043043923
Bhanuprathap <bhanuprathap.bussa@gmail.com>
2023-01-01 18:50:35 -0600

    Reading properties from application properties

---
+++ src/main/java/com/readconfig/readApplicationProperties/ConfigurationServices.java
@@ -0,0 +1,43 @@
+package com.readconfig.readApplicationProperties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "demo-service")
+@Component
+public class ConfigurationServices {
+
+    @Value("${url:http://www.google.com}")
+    private String url;
+
+    @Value("${username:default}")
+    private String username;
+
+    @Value("${key:defaultkey}")
+    private String key;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
