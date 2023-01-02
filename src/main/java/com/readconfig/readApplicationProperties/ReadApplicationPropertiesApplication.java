19ed219f8e94ab85ab7e6da81bc34089da191e90
Bhanuprathap <bhanuprathap.bussa@gmail.com>
2023-01-01 18:17:58 -0600

    Initial Project

---
+++ src/main/java/com/readconfig/readApplicationProperties/ReadApplicationPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.readconfig.readApplicationProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadApplicationPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadApplicationPropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readconfig/readApplicationProperties/ReadApplicationPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readconfig.readApplicationProperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadApplicationPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
